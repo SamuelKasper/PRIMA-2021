031535a7656cd81ef6b31c36f4bf9cfc816df612
SamuelKasper <64090949+SamuelKasper@users.noreply.github.com>
2021-03-18 15:49:15 +0100

    Neuer Code

---
+++ L01_FirstFudge/Test.js
@@ -0,0 +1,11 @@
+"use strict";
+var L01_FirstFudge;
+(function (L01_FirstFudge) {
+    var fc = FudgeCore;
+    window.addEventListener("load", hndLoad);
+    function hndLoad(_event) {
+        const canvas = document.querySelector("canvas");
+        fc.Debug.log(canvas);
+    }
+})(L01_FirstFudge || (L01_FirstFudge = {}));
+//# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,1 +1,13 @@
-console.log("Test");
+namespace L01_FirstFudge{
+    import fc = FudgeCore;
+
+    window.addEventListener("load", hndLoad);
+    export let viewport: fc.Viewport;
+
+    function hndLoad(_event: Event): void{
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        fc.Debug.log(canvas);
+
+    
+    }
+}
