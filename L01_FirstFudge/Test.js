6da73e5667023f5ce8b16423eae15acaede47bcd
SamuelKasper <64090949+SamuelKasper@users.noreply.github.com>
2021-03-25 17:19:23 +0100

    Zeitbasiert und Animation

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -4,10 +4,13 @@ var L01_FirstFudge;
     var fc = FudgeCore;
     //starten wenn Seite mit laden fertig ist
     window.addEventListener("load", init);
+    let viewport = new fc.Viewport();
+    let node = new fc.Node("Test");
     //Wird ausgeführt wenn Seite mit Laden fertig ist.
     function init(_event) {
-        let node = new fc.Node("Test");
         const canvas = document.querySelector("canvas");
+        //Transformationskomponente erzeugen um kasten zu verschieben
+        node.addComponent(new fc.ComponentTransform);
         let mesh = new fc.MeshQuad("Quad");
         //kompontente "ComponentMensh" verbindet Knoten "node" und Ressource "mesh"
         node.addComponent(new fc.ComponentMesh(mesh));
@@ -17,15 +20,24 @@ var L01_FirstFudge;
         node.addComponent(comMaterial);
         //mtx = matrix | Pivot matrix ist transformation gegenüber des Objektes - Welt matrix ist die transformation gegenüber der Welt
         console.log(node);
+        //Kamera erstellen und verschieben
         let comCamera = new fc.ComponentCamera();
         comCamera.mtxPivot.translateZ(3);
         comCamera.mtxPivot.rotateY(180);
         console.log(comCamera);
         //rechtshändiges koordinatensystem: z zu mir (vorne/hinten), x nach rechts/links, y nach oben/unten
-        let viewport = new fc.Viewport();
+        //viewport initialisieren
         viewport.initialize("Viewport", node, comCamera, canvas);
         viewport.draw();
         //Kamera nach vorne und um 180 grad drehen
+        fc.Loop.start(fc.LOOP_MODE.TIME_REAL, 60);
+        fc.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+    }
+    function update(_event) {
+        ///um Mesh (weclhes am Knoten hängt) zu drehen
+        let rotSpeed = 90 / 1000; //90 Grad pro Sek -> Frame rate unabhängig -> Zeitbasiert
+        node.getComponent(fc.ComponentMesh).mtxPivot.rotateZ(rotSpeed * fc.Loop.timeFrameReal);
+        viewport.draw();
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -3,13 +3,15 @@ namespace L01_FirstFudge{
 
     //starten wenn Seite mit laden fertig ist
     window.addEventListener("load", init);
-    export let viewport: fc.Viewport;
-
+    let viewport: fc.Viewport = new fc.Viewport();
+    let node: fc.Node = new fc.Node("Test");
+    
     //Wird ausgeführt wenn Seite mit Laden fertig ist.
-    function init(_event: Event): void{
-        let node: fc.Node = new fc.Node("Test");
+    function init(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        
+        //Transformationskomponente erzeugen um kasten zu verschieben
+        node.addComponent(new fc.ComponentTransform);
+
         let mesh: fc.MeshQuad = new fc.MeshQuad("Quad");
         //kompontente "ComponentMensh" verbindet Knoten "node" und Ressource "mesh"
         node.addComponent(new fc.ComponentMesh(mesh));
@@ -22,16 +24,26 @@ namespace L01_FirstFudge{
         //mtx = matrix | Pivot matrix ist transformation gegenüber des Objektes - Welt matrix ist die transformation gegenüber der Welt
         console.log(node);
 
+        //Kamera erstellen und verschieben
         let comCamera: fc.ComponentCamera = new fc.ComponentCamera();
         comCamera.mtxPivot.translateZ(3);
         comCamera.mtxPivot.rotateY(180);
         console.log(comCamera);
 
         //rechtshändiges koordinatensystem: z zu mir (vorne/hinten), x nach rechts/links, y nach oben/unten
-        let viewport: fc.Viewport = new fc.Viewport();
+        //viewport initialisieren
         viewport.initialize("Viewport", node, comCamera, canvas);
         viewport.draw();
 
         //Kamera nach vorne und um 180 grad drehen
+        fc.Loop.start(fc.LOOP_MODE.TIME_REAL, 60);
+        fc.Loop.addEventListener(fc.EVENT.LOOP_FRAME, update);
+    }
+
+    function update(_event: Event): void {
+        ///um Mesh (weclhes am Knoten hängt) zu drehen
+        let rotSpeed: number = 90 / 1000; //90 Grad pro Sek -> Frame rate unabhängig -> Zeitbasiert
+        node.getComponent(fc.ComponentMesh).mtxPivot.rotateZ(rotSpeed * fc.Loop.timeFrameReal); 
+        viewport.draw();
     }
 }
