323e017c8b6e9d3549286f91fb1190daff75f149
SamuelKasper <64090949+SamuelKasper@users.noreply.github.com>
2021-03-25 16:09:25 +0100

    First Fudge Final

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -2,10 +2,30 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var fc = FudgeCore;
-    window.addEventListener("load", hndLoad);
-    function hndLoad(_event) {
+    //starten wenn Seite mit laden fertig ist
+    window.addEventListener("load", init);
+    //Wird ausgeführt wenn Seite mit Laden fertig ist.
+    function init(_event) {
+        let node = new fc.Node("Test");
         const canvas = document.querySelector("canvas");
-        fc.Debug.log(canvas);
+        let mesh = new fc.MeshQuad("Quad");
+        //kompontente "ComponentMensh" verbindet Knoten "node" und Ressource "mesh"
+        node.addComponent(new fc.ComponentMesh(mesh));
+        //material erzeugen: Name, shader erzeugt Pixel, Farbe
+        let material = new fc.Material("Material", fc.ShaderUniColor, new fc.CoatColored(new fc.Color(1, 1, 1, 1)));
+        let comMaterial = new fc.ComponentMaterial(material);
+        node.addComponent(comMaterial);
+        //mtx = matrix | Pivot matrix ist transformation gegenüber des Objektes - Welt matrix ist die transformation gegenüber der Welt
+        console.log(node);
+        let comCamera = new fc.ComponentCamera();
+        comCamera.mtxPivot.translateZ(3);
+        comCamera.mtxPivot.rotateY(180);
+        console.log(comCamera);
+        //rechtshändiges koordinatensystem: z zu mir (vorne/hinten), x nach rechts/links, y nach oben/unten
+        let viewport = new fc.Viewport();
+        viewport.initialize("Viewport", node, comCamera, canvas);
+        viewport.draw();
+        //Kamera nach vorne und um 180 grad drehen
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,13 +1,37 @@
 namespace L01_FirstFudge{
     import fc = FudgeCore;
 
-    window.addEventListener("load", hndLoad);
+    //starten wenn Seite mit laden fertig ist
+    window.addEventListener("load", init);
     export let viewport: fc.Viewport;
 
-    function hndLoad(_event: Event): void{
+    //Wird ausgeführt wenn Seite mit Laden fertig ist.
+    function init(_event: Event): void{
+        let node: fc.Node = new fc.Node("Test");
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        fc.Debug.log(canvas);
+        
+        let mesh: fc.MeshQuad = new fc.MeshQuad("Quad");
+        //kompontente "ComponentMensh" verbindet Knoten "node" und Ressource "mesh"
+        node.addComponent(new fc.ComponentMesh(mesh));
 
-    
+        //material erzeugen: Name, shader erzeugt Pixel, Farbe
+        let material: fc.Material = new fc.Material("Material", fc.ShaderUniColor, new fc.CoatColored(new fc.Color(1, 1, 1, 1)));
+        let comMaterial: fc.ComponentMaterial = new fc.ComponentMaterial(material);
+        node.addComponent(comMaterial);
+
+        //mtx = matrix | Pivot matrix ist transformation gegenüber des Objektes - Welt matrix ist die transformation gegenüber der Welt
+        console.log(node);
+
+        let comCamera: fc.ComponentCamera = new fc.ComponentCamera();
+        comCamera.mtxPivot.translateZ(3);
+        comCamera.mtxPivot.rotateY(180);
+        console.log(comCamera);
+
+        //rechtshändiges koordinatensystem: z zu mir (vorne/hinten), x nach rechts/links, y nach oben/unten
+        let viewport: fc.Viewport = new fc.Viewport();
+        viewport.initialize("Viewport", node, comCamera, canvas);
+        viewport.draw();
+
+        //Kamera nach vorne und um 180 grad drehen
     }
 }
